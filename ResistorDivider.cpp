dddf9263ba3c907c9e076ae18c0203d562d8d8b6
Kamil <40038293+Kamil4567@users.noreply.github.com>
2024-01-17 22:54:15 +0100

    Create ResistorDivider class and constructors

--- ResistorDivider.cpp
+++ ResistorDivider.cpp
@@ -1,1 +1,17 @@
+#include "ResistorDivider.h"
 
+ResistorDivider::ResistorDivider(){
+
+}
+
+ResistorDivider::ResistorDivider(float resistor1, float resistor2){
+    this->R1 = resistor1;
+    this->R2 = resistor2;
+}
+
+ResistorDivider::ResistorDivider(float resistor1, float resistor2, int pinNumber, int adcMaxValue){
+    this->R1 = resistor1;
+    this->R2 = resistor2;
+    this->pinNumber = pinNumber;
+    this->adcMaxValue = adcMaxValue;
+}
--- ResistorDivider.h
+++ ResistorDivider.h
@@ -1,7 +1,19 @@
 #ifndef RESISTOR_DIVIDER_H
 #define RESISTOR_DIVIDER_H
 
+#include <Arduino.h>
 
+class ResistorDivider{
+    private:
+        float R1 = 1;
+        float R2 = 1;
+        int pinNumber = -1;
+        int adcMaxValue = 1024;
+    public:
+        ResistorDivider();
+        ResistorDivider(float resistor1, float resistor2);
+        ResistorDivider(float resistor1, float resistor2, int pinNumber, int maxValue);
+};
 
 
 
